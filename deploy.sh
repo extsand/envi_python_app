465fdbacfa445261bd6d6094a8a03df9716669d5
Alexander <extsand@gmail.com>
2022-01-23 01:55:15 +0200

    readme

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo '================================================='
+echo '===========Docker Buil Container================='
+echo '================================================='
+docker build --build-arg app_username=$APP_USERNAME \
+ 						 --build-arg app_db=$APP_DB \
+						 --build-arg app_password=$APP_PASSWORD \
+  						-t env_transmitting:0.1 ./.
+echo '================================================='
+echo '===========Running Application==================='
+echo '================================================='
+
+docker run -it -p 80:5000 env_transmitting:0.1
