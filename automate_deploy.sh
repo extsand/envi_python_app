b15ce0345fe7f1d99da8c14394ba1a5f39254b61
Alexander <extsand@gmail.com>
2022-01-23 00:34:53 +0200

    readme

--- automate_deploy.sh
+++ automate_deploy.sh
@@ -2,9 +2,9 @@
 echo 'Automate deploy for Envi.Transmitting'
 
 bash ./set_env.sh
-docker build --build-arg app_username=$(APP_USERNAME) \
- 						 --build-arg app_db=$(APP_DB) \
-						 --build-arg app_password=$(APP_PASSWORD) \
+docker build --build-arg app_username=$APP_USERNAME \
+ 						 --build-arg app_db=$APP_DB \
+						 --build-arg app_password=$APP_PASSWORD \
   						-t env_transmitting:0.1 ./.
 
 docker run -it -p 80:5000 env_transmitting:0.1
