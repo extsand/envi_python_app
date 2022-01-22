5e682baecd12fb764864ffcd91c40dff6e32ea8d
Alexander <extsand@gmail.com>
2022-01-23 01:18:10 +0200

    readme

--- automate_deploy.sh
+++ automate_deploy.sh
@@ -6,5 +6,8 @@ docker build --build-arg app_username=$APP_USERNAME \
  						 --build-arg app_db=$APP_DB \
 						 --build-arg app_password=$APP_PASSWORD \
   						-t env_transmitting:0.1 ./.
+echo '================================================='
+echo '===========Running Application==================='
+echo '================================================='
 
-docker run -it -p 80:5000 env_transmitting:0.1
+docker run -it -p 8081:5000 env_transmitting:0.1
